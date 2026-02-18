d6943b4a579538dc4b30f03138d75b151670b474
WIKKI <uzkingshark@gmail.com>
2026-02-18 14:45:54 +0500

    zebra label: lower text/barcode and enlarge item title

--- scale/zebra_monitor_epc.go
+++ scale/zebra_monitor_epc.go
@@ -55,13 +55,13 @@ func buildRFIDEncodeCommand(epc, qtyText, itemName string) (string, error) {
 		"^MMT\n" +
 		"^RS8,,,1,N\n" +
 		fmt.Sprintf("^RFW,H,,,A^FD%s^FS\n", norm) +
-		"^FO8,26^A0N,30,24^FB760,1,0,L,0\n" +
+		"^FO8,38^A0N,34,28^FB760,1,0,L,0\n" +
 		fmt.Sprintf("^FDITEM: %s^FS\n", item) +
-		"^FO8,80^A0N,44,38\n" +
+		"^FO8,98^A0N,44,38\n" +
 		fmt.Sprintf("^FDQTY: %s^FS\n", qty) +
-		"^FO8,142^A0N,30,24^FB760,1,0,L,0\n" +
+		"^FO8,162^A0N,32,26^FB760,1,0,L,0\n" +
 		fmt.Sprintf("^FDEPC: %s^FS\n", sanitizeZPLText(norm)) +
-		"^FO8,198^BY3,2,44^BCN,44,N,N,N\n" +
+		"^FO8,220^BY3,2,44^BCN,44,N,N,N\n" +
 		fmt.Sprintf("^FD%s^FS\n", sanitizeZPLText(norm)) +
 		"^PQ1\n" +
 		"^XZ\n", nil
