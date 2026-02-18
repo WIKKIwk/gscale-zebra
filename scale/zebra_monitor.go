568dbd4fdb14edd8cb8ff44541b890e918b32d0e
WIKKI <uzkingshark@gmail.com>
2026-02-18 09:09:41 +0500

    chore(workspace): add go.work and zebra monitor compatibility file

---
+++ scale/zebra_monitor.go
@@ -0,0 +1,8 @@
+package main
+
+// Zebra monitor kodi modullarga ajratilgan:
+// - zebra_monitor_types.go
+// - zebra_monitor_status.go
+// - zebra_monitor_actions.go
+// - zebra_monitor_transport.go
+// - zebra_monitor_epc.go
